5b1444e222dae78ed6a7d6368ba5ed9ba45240ea
Iaroslav Uliantcev <doudou239@gmail.com>
2025-10-07 21:56:33 +0200

    Add support for importing `.jpg` files in TypeScript definitions.

--- custom.d.ts
+++ custom.d.ts
@@ -9,6 +9,11 @@ declare module '*.png' {
   export default content;
 }
 
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
 declare module '*.css' {
   interface IClassNames {
     [className: string]: string;
