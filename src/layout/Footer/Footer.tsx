ee2b69c1b8f2f20b04159a8d05e6277f8dfb3625
Iaroslav Uliantcev <doudou239@gmail.com>
2025-10-08 10:54:13 +0200

    Update Footer with LinkedIn link, custom logo.

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import LinkedInLogo from '@/assets/icons/linkedin.svg';
+import IULogo from '@/assets/images/logo-IU.svg';
+
 import * as styles from './Footer.module.scss';
 
+const LINKEDIN_LINK = 'https://www.linkedin.com/in/i-uliantsev/';
+
 const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <IULogo width={50} height={50} />
+      <div className={styles.line}>
+        <p>2025, Iaroslav Uliantsev</p>
+        <a href={LINKEDIN_LINK} target="_blank" rel="noopener noreferrer">
+          <LinkedInLogo />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
